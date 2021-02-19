03c7801d2bc9a408f5a5bf08ffafdbc9be6a1bd1
ROCIO PE�A <1720110352@utectulancingo.edu.mx>
2021-02-19 06:18:14 +0000

    primera

---
+++ SEMANA_4/dia_a_segundos.py
@@ -0,0 +1,12 @@
+class Dias_segundos():
+
+  def __init__(self):
+    pass
+
+  def convercion(self,dia):
+    for dias in range(4):
+      con = dia * 86400
+      print(dias)
+
+resultado = Dias_segundos()
+resultado.convercion(7)
---
+++ SEMANA_5/asistencia.py
@@ -0,0 +1,13 @@
+class Asistencia():
+
+  materia = None
+
+  def bucle(self):
+    repetir = "s"
+    while repetir == "s":
+      print("hola")
+      repetir == input("Repetir otra vez (s/n)") 
+
+hi = Asistencia()
+hi.materia = "buuuuuuuu"
+hi.bucle()
