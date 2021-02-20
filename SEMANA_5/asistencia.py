bc90b1262c56d5b58909e7d433919209306073f2
ROCIO PE�A <1720110352@utectulancingo.edu.mx>
2021-02-20 07:26:46 +0000

    segundo

--- SEMANA_5/asistencia.py
+++ SEMANA_5/asistencia.py
@@ -1,13 +1,30 @@
 class Asistencia():
 
-  materia = None
-
   def bucle(self):
     repetir = "s"
     while repetir == "s":
-      print("hola")
-      repetir == input("Repetir otra vez (s/n)") 
+
+      materia = input("Materia: ")
+      numero_alumno = input("Numero de alumnos: ")
+      nombre = input("Nombre del alumno: ")
+      num_clase = int(input("Numero de clases: ")) 
+      num_faltas = int(input("Numero de faltas: "))
+      num_retraso = int(input("Numero de retraso:"))
+      #if num_retraso >= 3:
+      # uno = 1
+      #else:
+       # cero = 0
+
+      resta = num_clase - num_faltas
+      multi = 100/num_clase
+      porcentaje = resta*multi
+      print("Porcentaje: ", porcentaje)
+      if porcentaje > 80:
+        print("Resultado: Tienes derecho")
+      else:
+        print("Resultado: No tienes derecho")
+
+      repetir = input("Otra evaluacion (s/n)") 
 
 hi = Asistencia()
-hi.materia = "buuuuuuuu"
 hi.bucle()
